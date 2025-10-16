847f61dc6f91f868ab2fd208097e1854bd30a035
lsya58 <milocbst369@gmail.com>
2025-10-16 10:20:14 +0900

    ホームページ作成最初

---
+++ next-book/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header style={{
+            position: "sticky", top: 0, background: "#0f172a", color: "#fff",
+            borderBottom: "1px solid #223", zIndex: 10
+        }}>
+            <div style={{
+                maxWidth: 960, margin: "0 auto", display: "flex",
+                alignItems: "center", justifyContent: "space-between", padding: "12px 16px"
+            }}>
+                
+            </div>
+        </header>
+    )
+} 
